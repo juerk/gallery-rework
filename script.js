707ff1f8b7b3fdcc5fcd47590586e659a043cf05
juerk <wuerkerrs06@gmail.com>
2022-12-29 10:50:05 -0700

    Added image pop up functionality. Re-styled gallery.

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
+// main drop downs
 const dropDown = document.getElementById('box-dropdown')
 const mirrorsDropped = document.getElementById('mirrors-dropped')
 let dropDownSymbol = document.getElementById('box-dropdown')
 mirrorsDropped.style.display = 'none';
-dropDownSymbol.innerHTML = '+'
+// dropDownSymbol.innerHTML = '+'
 
-
-
-
-
-// function toggleDropDown() {
-//   return (mirrorsDropped.style.display === 'none') ? mirrorsDropped.style.display = 'block' : mirrorsDropped.style.display = 'none'
-// }
 
 function toggleDropDown() {
   if (mirrorsDropped.style.display === 'none') {
@@ -22,6 +16,115 @@ function toggleDropDown() {
   }
 }
 
+// modal land
+let ttlModals = 4;
 
-  
+function showModal(cellID) {
+  for (let i = 0; i < ttlModals; i++) {
+    let allModals = `modalcontent${[i]}`
+    let allModalsID = document.getElementById(allModals)
+    allModalsID.style.display = 'none'
+  }
+  let selectedID = `modalcontent${cellID}`
+  let modalSelected = document.getElementById(selectedID)
+  modalSelected.style.display = 'block'
+}
 
+function allModalGen() {
+  let allModalsArray = []
+  for (let i = 0; i < ttlModals; i++) {
+    let allModals = `modalcontent${[i]}`
+    let allModalsID = document.getElementById(allModals)
+    allModalsArray.push(allModalsID)
+  }
+  return allModalsArray
+}
+let allModalsArray = allModalGen()
+
+function closeModal() {
+  for (let i = 0; i < ttlModals; i++) {
+    allModalsArray[i].style.display = 'none'
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function toggleDropDown() {
+//   return (mirrorsDropped.style.display === 'none') ? mirrorsDropped.style.display = 'block' : mirrorsDropped.style.display = 'none'
+// }
+
+
+
+// let testDiv = document.getElementById('testdiv')
+// let testDivImg = document.getElementById('testdivimg')
+
+
+
+
+// function toggleGalleryDrop() {
+//   if (zoom.style.display === 'none') {
+//     zoom.style.display = 'block'
+//     testDivImg.style.width = '360px'
+//     testDivImg.style.height = '360px'
+//   } else {
+//     zoom.style.display = 'none'
+//     testDivImg.style.width = ''
+//     testDivImg.style.height = ''
+//   }
+// }
+
+
+
+
+// let expanded = false
+// let zoom = document.getElementById('zoom0')
+// zoom.style.display = 'none'
+
+// function imageSelected(cellID) {
+//   console.log("image selected func")
+//   imageChosen = document.getElementById(cellID);
+//   let zoomSelector = `zoom${cellID}`
+//   let selectedZoom = document.getElementById(zoomSelector)
+//   console.log(selectedZoom)
+//   imageExpander(imageChosen) 
+//   }
+
+//   function imageExpander(imageChosen) {
+//     expanded = !expanded
+//     if (expanded) {
+//       console.log("expanded")
+//       imageChosen.classList.add("expand")
+//       imageChosen.classList.remove("collapse")
+//     } else {
+//       console.log("collapsed")
+//       imageChosen.classList.add("collapse")
+//       imageChosen.classList.remove("expand")
+//     }
+//   }
